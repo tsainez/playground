acd7e1ae1372797db1a920d3e814246a9a1e364e
Tony Sainez <tsainez@gmail.com>
2022-11-29 12:05:07 -0800

    Initial Commit

---
+++ Source/playground/playgroundGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "playgroundGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLAYGROUND_API AplaygroundGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
